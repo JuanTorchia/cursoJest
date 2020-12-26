72475e1e78e6ff11a2b4284e8246972b1293910f
Juan Torchia <j.s.torchia@gmail.com>
2020-12-25 21:57:27 -0300

    Curso JEST

---
+++ __test__/math.test.js
@@ -0,0 +1,16 @@
+import { sumar, multiplicar, restar, dividir } from '../maths.js';
+
+describe('Calculos matematicos', () =>{
+    test('Prueba de sumas', ()=> {
+        expect(sumar(1,1)).toBe(2);
+    });
+    test('Multiplicar ', () => {
+        expect(multiplicar(2,2)).toBe(4);
+    });
+    test('Restar ', () => {
+        expect(restar(2,1)).toBe(1);
+    });
+    test('Dividir ', () => {
+        expect(dividir(6,3)).toBe(2);
+    });
+});
